722a9df166623dc4b414bd314a604676010f08bf
guilhermealvesc <guilherme.092011@hotmail.com>
2025-01-31 17:38:21 -0300

    fix: update authorization header to use x-api-key instead of Bearer token

--- client.go
+++ client.go
@@ -187,7 +187,7 @@ func (c *Client) setCommonHeaders(req *http.Request) {
 		req.Header.Set(AzureAPIKeyHeader, c.config.authToken)
 	} else if c.config.authToken != "" {
 		// OpenAI or Azure AD authentication
-		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", c.config.authToken))
+		req.Header.Set("x-api-key", c.config.authToken)
 	}
 	if c.config.OrgID != "" {
 		req.Header.Set("OpenAI-Organization", c.config.OrgID)
